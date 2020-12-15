645abfb90cb768ba5182af4787b9707684d5c62b
TalitaBorges <talitaramosborges.arte@gmail.com>
2020-12-15 13:06:24 +0000

    CRUD de produtos para a web

--- LojaCET49/Dados/Entidades/Produto.cs
+++ LojaCET49/Dados/Entidades/Produto.cs
@@ -10,6 +10,8 @@ namespace LojaCET49.Dados.Entidades
 
         public string Nome { get; set; }
 
+        [Display(Name = "Preço")]
+
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Preco { get; set; }
 
